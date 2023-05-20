b81e14c0ad26a3382b249bef63a8697a352d40b9
Hyung Kyu <onlymuk@naver.com>
2023-05-20 14:29:42 +0900

    six commit

--- js-1/practice.js
+++ js-1/practice.js
@@ -29,15 +29,38 @@ pass(70, 70);
 pass(30, 100);
 pass(50, 50);
 
-function threeSixNine(num) {
-  if (num % 10 == 6 || num % 10 == 9 || num % 10 == 3) {
-    console.log("박수");
-  } else {
-    console.log("패스");
-  }
-  if (num > 100) {
-    alert("장난같나");
-  }
+// function threeSixNine(num) {
+//   if (num % 10 == 6 || num % 10 == 9 || num % 10 == 3) {
+//     console.log("박수");
+//   } else {
+//     console.log("패스");
+//   }
+//   if (num > 100) {
+//     alert("장난같나");
+//   }
+// }
+
+// threeSixNine(101);
+
+let 나이 = 26;
+const 지역 = "이천";
+
+// return kim, 0;
+
+var 예금액 = 10000;
+var 미래예금액 = 0;
+
+if (예금액 < 50000) {
+  미래예금액 = Math.ceil(1.15 ** 2 * 예금액);
+} else {
+  미래예금액 = Math.ceil(1.2 ** 2 * 예금액);
 }
 
-threeSixNine(101);
+console.log(미래예금액);
+console.log(미래예금액);
+
+var first = 360;
+var total = 0;
+// total = first + first * (2 / 3) + first * (2 / 3) ** 2;
+total = Math.ceil(360 / (1 - 2 / 3));
+console.log(total);
